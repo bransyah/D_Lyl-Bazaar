e407bd0691ed118a4056174a75e190a73b7c3381
mirahay17 <57434306+mirahay17@users.noreply.github.com>
2020-01-08 00:11:19 -0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -5,3 +5,5 @@ session_start(); // function start (mulai) source code.
 session_unset("user"); // untuk menghapus variabel pada session.
 
 header("Location: index.php"); // untuk mengarahkan ke laman index setelah user log out.
+
+?>
